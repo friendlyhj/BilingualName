19cb0449e1d074de3a54681126206ff5bb5fc74e
youyihj <youyi580@qq.com>
2020-11-04 21:16:10 +0800

    version 1.1

--- src/main/java/youyihj/bilingualname/BilingualName.java
+++ src/main/java/youyihj/bilingualname/BilingualName.java
@@ -10,7 +10,7 @@ public class BilingualName
 {
     public static final String MODID = "bilingualname";
     public static final String NAME = "Bilingual Name";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     private static Logger logger;
 
