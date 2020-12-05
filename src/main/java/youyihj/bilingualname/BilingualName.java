69ab2688319af7a9af744cd3975e9db95558793e
youyihj <youyi580@qq.com>
2020-12-05 13:20:59 +0800

    unused import

--- src/main/java/youyihj/bilingualname/BilingualName.java
+++ src/main/java/youyihj/bilingualname/BilingualName.java
@@ -1,10 +1,6 @@
 package youyihj.bilingualname;
 
-import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = BilingualName.MODID, name = BilingualName.NAME, version = BilingualName.VERSION, clientSideOnly = true)
 public class BilingualName
